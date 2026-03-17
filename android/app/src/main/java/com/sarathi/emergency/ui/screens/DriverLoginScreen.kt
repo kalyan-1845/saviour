17c7e32245ef421a3e996579142b3d8d58e9bbd3
kalyan-1845 <prsnlkalyan2gmail.com>
2026-03-17 12:26:47 +0530

    driver login and registration UI screens.

--- android/app/src/main/java/com/sarathi/emergency/ui/screens/DriverLoginScreen.kt
+++ android/app/src/main/java/com/sarathi/emergency/ui/screens/DriverLoginScreen.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.sarathi.emergency.data.models.DriverLoginRequest
+import com.sarathi.emergency.data.models.LoginRequest
 import com.sarathi.emergency.ui.components.GlowButton
 import com.sarathi.emergency.ui.components.GlowVariant
 import com.sarathi.emergency.ui.theme.*
